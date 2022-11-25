52e06ac9604f97bcd54bcb866937a68816ec16be
dukekon <19833038@qq.com>
2022-11-25 23:44:59 +0800

    完成首页布局 | complete home layout

--- src/components/home-head-info/home-head-info.vue
+++ src/components/home-head-info/home-head-info.vue
@@ -1,18 +1,18 @@
 <template>
   <tm-row>
-    <tm-col v-for="(item, i) in values" :key="i" transprent>
+    <tm-col class="flex-1" v-for="(item, i) in values" :key="i" transprent>
       <tm-text :font-size="32" color="white" :label="item" />
     </tm-col>
   </tm-row>
   <tm-row>
     <tm-col v-for="(item, i) in titles" :key="i" transprent>
-      <tm-text :font-size="32" color="white" :label="item" />
+      <tm-text :font-size="28" color="white" :label="item" />
     </tm-col>
   </tm-row>
 </template>
 
 <script lang="ts" setup>
-const values = [13, 7, 112]
+const values = [4, 5, 10]
 const titles = ['现有租客', '现有租客', '历史租客']
 </script>
 
