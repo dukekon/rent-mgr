0b8d14010f36db554858232aeedc951f95e4bc52
dukekon <19833038@qq.com>
2022-11-26 04:29:53 +0800

    优化首页头部视图 | optimize home header view

--- src/components/home-top-notice/home-top-notice.vue
+++ src/components/home-top-notice/home-top-notice.vue
@@ -1,5 +1,5 @@
 <template>
-  <tm-sheet _class="flex-row flex-between" :margin="[0, 0]" :padding="[0, 32]" :round="4" :shadow="2">
+  <tm-sheet _class="flex-row flex-between" :margin="[0, 0]" :padding="[0, 28]" :round="4" :shadow="2">
     <view class="flex flex-1 flex-row flex-row-bottom-center text-weight-b" :class="i === 1 && 'border-r-1'"
       v-for="i in 2" :key="i">
       <tm-text class="mr-16 relative t-4" :font-size="48" :color="i === 1 ? 'green' : 'red'" :label="num[i - 1]" />
