edc0b8c30ccde2e23cc55c091d42e4ddb7713043
dukekon <19833038@qq.com>
2022-11-25 23:16:37 +0800

    完成首页布局 | complete home layout

--- src/components/home-top-icons/home-top-icons.vue
+++ src/components/home-top-icons/home-top-icons.vue
@@ -1,7 +1,7 @@
 <template>
   <tm-sheet _class="flex-row flex-around" :margin="[0, 20]" :padding="[0, 32]" :round="4" :shadow="2">
     <view class=" flex-col flex-center" v-for="(item, index) in icons" :key="index">
-      <tm-icon _class="mb-20" :name="item.icon" :font-size="96" />
+      <tm-icon _class="mb-24" :name="item.icon" :font-size="96" />
       <tm-text :label="item.text" />
     </view>
   </tm-sheet>
@@ -19,7 +19,7 @@ const icons = [
   },
   {
     icon: '../../static/icons/water&electricity@2x.png',
-    text: '抄表'
+    text: '水电'
   },
   {
     icon: '../../static/icons/feature@2x.png',
--- src/components/home-top-notice/home-top-notice.vue
+++ src/components/home-top-notice/home-top-notice.vue
@@ -2,7 +2,7 @@
   <tm-sheet _class="flex-row flex-between" :margin="[0, 0]" :padding="[0, 32]" :round="4" :shadow="2">
     <view class="flex flex-1 flex-row flex-row-bottom-center text-weight-b" :class="i === 1 && 'border-r-1'"
       v-for="i in 2" :key="i">
-      <tm-text class="mr-8 relative t-4" :font-size="48" :color="i === 1 ? 'green' : 'red'" :label="num[i - 1]" />
+      <tm-text class="mr-16 relative t-4" :font-size="48" :color="i === 1 ? 'green' : 'red'" :label="num[i - 1]" />
       <tm-text :font-size="32" :label="text[i - 1]" />
     </view>
   </tm-sheet>
